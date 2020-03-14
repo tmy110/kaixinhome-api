990a972ea2494a573ef4b0c39289f4d8a64d7821
“田满意119” <tmy18361036456@126.com>
2020-03-14 17:39:36 +0800

    热修复测试二

--- src/main/java/com/kaixin8848/home/web/favorites/controller/FavoritesController.java
+++ src/main/java/com/kaixin8848/home/web/favorites/controller/FavoritesController.java
@@ -28,11 +28,14 @@ import java.util.Date;
 import java.util.List;
 
 /**
+ * 热修复测试二
  * Created by tmy on 2019/12/08.
  * edit by 田满意119 5.0
  * 1234
  * 5678
  * test develop分支  我还想测试一下
+ * 热修复分支 热修复测试三
+ * 你好吗热修复
  */
 @RestController
 @RequestMapping("/favorites")
